09a61300fd78f7f9e98bdac36db72b71d5eef5a2
Andres <andres.ottonello@gmail.com>
2021-02-26 12:30:42 -0300

    AFO func. listar en funcionalidadesTareas.js

--- funcionalidadesTareas.js
+++ funcionalidadesTareas.js
@@ -4,39 +4,41 @@ let operacionesArchivo = require('./operacionesArchivo.js');
 let concesionaria = {
   autos: operacionesArchivo.leerArchivoJson(),
 
-  listar: 
+  listar: () => {},
       // cuando se llama a este metodo se imprimen todos los autos en autos.json
 
 
-  buscarAuto: 
+  buscarAuto: (patente) => {
+    return autos.filter(a => a.patente = patente);
+  },
     // cuando se llama a este metodo se imprimen todos los datos del auto con de la patente dada. 
     //Sino esta, devuelve null
 
 
-  venderAuto: 
+  venderAuto: () => {},
     // cuando se llama a este metodo se sobrescribe el objeto de autos, cambiando este auto en particular a vendido
   
 
-  autosParaLaVenta: 
+  autosParaLaVenta: () => {},
     // cuando se llama a este metodo se imprimen todos los autos en autos.json que su estado vendido es false
     
 
-  autosNuevos: 
+  autosNuevos: () => {},
     // cuando se llama a este metodo se imprimen todos los autos en autos.json que su estado vendido es false 
     //y sus km son cero
     // para determinar cuales no estan vendidos se aprovecha como callback del metodo anterior.
    
 
-  listaDeVentas: 
+  listaDeVentas: () => {},
     // cuando se llama a este metodo se imprime un array de precios de autos cuyo estado vendido es true
     
 
-  totalDeVentas: 
+  totalDeVentas: () => {},
     // cuando se llama a este se suman los precios de todos los autos vendidos
     // los precios de los autos vendidos se los trae usando como callback el metodo anterior
  
 
-  puedeComprar: 
+  puedeComprar: () => {},
     // este metodo recibe un objeto auto y un objeto persona, no se puede llamar directamente.
     // dice true o false si esa persona puede comprar ese auto
     // lo que hace es primero chequear si el precio del auto esta por debajo de lo que la persona esta dispuesta a pagar
@@ -44,10 +46,9 @@ let concesionaria = {
     // si y solo si ambas condiciones son true devuelve true, en otro caso devuelve false
    
 
-  autosQuePuedeComprar: 
+  autosQuePuedeComprar: () => {},
     // dada una persona este metodo trae todos los autos que esa persona puede pagar
 
- 
 }
 
 module.exports = concesionaria;
